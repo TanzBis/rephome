31c5da02192a0b06374b953c89fa2c5807841526
Bisultanova Tanzila <tanzbis81@gmail.com>
2021-10-01 00:24:25 +0300

    first homework

---
+++ script.js
@@ -0,0 +1,15 @@
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+console.log("Any text")
+
+alert("message") // внутри редактора кода работать не будет
+
+
+
+
+
