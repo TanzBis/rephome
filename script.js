0b0a403b014df8f38a7d92e68e5a8dbc1a641984
Bisultanova Tanzila <tanzbis81@gmail.com>
2021-10-01 17:50:45 +0300

    Lesson2 done

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let title = "MyCalc";
-console.log(typeof title)
-let screens = "простые, сложные, интерактивные";
+console.log(typeof title);
+let screens = "Простые, Сложные, Интерактивные";
+console.log(screens.length);
+console.log(screens.toLowerCase());
+const forScreens = screens.split(", ");
+console.log(forScreens);
 let screenPrice = 1;
+console.log("Стоимость верстки экранов:", screenPrice, "рублей");
 let rollback = 20;
 let fullPrice = 250000;
+console.log(fullPrice * (rollback/100));
+console.log("Стоимость разработки сайта:", fullPrice, "рублей");
 console.log(typeof fullPrice);
 let adaptive = true;
 console.log(typeof adaptive);
+
 
 console.log("Any text")
 
