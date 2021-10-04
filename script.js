26b168e319017ea6b983ef4fc6ba9e043ab56e1d
Bisultanova Tanzila <tanzbis81@gmail.com>
2021-10-04 21:18:56 +0300

    lesson3

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-let title = "MyCalc";
+'use strict'
+
+let title = prompt("Как называется ваш проект?");
+console.log(String(title));
 console.log(typeof title);
-let screens = "Простые, Сложные, Интерактивные".toLowerCase();
-console.log(screens.length);
+let screens = prompt("Какие типы экранов нужно разработать?");
+console.log(String(screens));
 const forScreens = screens.split(", ");
 console.log(forScreens);
-let screenPrice = 1;
-console.log("Стоимость верстки экранов:", screenPrice, "рублей");
-let rollback = 20;
-let fullPrice = 250000;
-console.log(fullPrice * (rollback/100));
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+console.log(Number(screenPrice));
+let adaptive = confirm("Нужен ли адаптив?");
+console.log((adaptive));
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service1);
+let servicePrice1 = +prompt("Cколько это будет стоить?");
+console.log(servicePrice1);
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service2);
+let servicePrice2 = +prompt("Cколько это будет стоить?");
+console.log(servicePrice2);
+let rollback = 70;
+let fullPrice = (screenPrice + servicePrice1 + servicePrice2)*((rollback/100));
+console.log(Math.ceil(fullPrice));
 console.log("Стоимость разработки сайта:", fullPrice, "рублей");
-console.log(typeof fullPrice);
-let adaptive = true;
-console.log(typeof adaptive);
-
-
-console.log("Any text")
+if (fullPrice > 30000) 
+{
+  alert("Даем скидку в 10%");
+} else if (15000 < fullPrice && fullPrice < 30000) {
+  alert("Даем скидку в 5%");
+} else if (15000 < fullPrice &&  fullPrice > 0 ) {
+  alert("Скидка не предусмотрена");
+} else if(fullPrice > 0) {
+  alert("что-то пошло не так");
+} else if(fullPrice == 30000) {
+  alert("Обойдется в 30000");
+} else if(fullPrice == 15000) {
+  alert("Обойдется");
+} else if(fullPrice == 0) {
+  alert("жаль, что мы не работаем бесплатно");
+} else {
+  alert(fullPrice);
+}
+console.log("Any text");
 
 alert("message") // внутри редактора кода работать не будет
 
