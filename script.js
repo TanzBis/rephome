bfd6ef69186f81acbcebe701d374d32aee94d0bc
Bisultanova Tanzila <tanzbis81@gmail.com>
2021-10-05 00:43:03 +0300

    Let the third lesson be right

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 'use strict'
-
 let title = prompt("Как называется ваш проект?");
-console.log(String(title));
+console.log((title));
 console.log(typeof title);
-let screens = prompt("Какие типы экранов нужно разработать?");
-console.log(String(screens));
+let screens = prompt("Какие типы экранов нужно разработать?".toLowerCase());
 const forScreens = screens.split(", ");
+console.log((screens));
 console.log(forScreens);
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
-console.log(Number(screenPrice));
+console.log((screenPrice));
 let adaptive = confirm("Нужен ли адаптив?");
 console.log((adaptive));
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
@@ -20,29 +19,22 @@ console.log(service2);
 let servicePrice2 = +prompt("Cколько это будет стоить?");
 console.log(servicePrice2);
 let rollback = 70;
-let fullPrice = (screenPrice + servicePrice1 + servicePrice2)*((rollback/100));
-console.log(Math.ceil(fullPrice));
+let fullPrice = (screenPrice + servicePrice1 + servicePrice2);
+let servicePercentprice = fullPrice - (rollback/100);
+console.log(Math.ceil(servicePercentprice));
 console.log("Стоимость разработки сайта:", fullPrice, "рублей");
-if (fullPrice > 30000) 
+if (fullPrice > 30000)
 {
-  alert("Даем скидку в 10%");
-} else if (15000 < fullPrice && fullPrice < 30000) {
-  alert("Даем скидку в 5%");
-} else if (15000 < fullPrice &&  fullPrice > 0 ) {
-  alert("Скидка не предусмотрена");
+  console.log("Даем скидку в 10%");
+} else if (fullPrice <= 30000 && fullPrice > 15000) {
+    console.log("Даем скидку в 5%");
+} else if (15000 <= fullPrice &&  fullPrice > 0 ) {
+    console.log("Скидка не предусмотрена");
 } else if(fullPrice > 0) {
-  alert("что-то пошло не так");
-} else if(fullPrice == 30000) {
-  alert("Обойдется в 30000");
-} else if(fullPrice == 15000) {
-  alert("Обойдется");
-} else if(fullPrice == 0) {
-  alert("жаль, что мы не работаем бесплатно");
-} else {
-  alert(fullPrice);
+    console.log("что-то пошло не так");
 }
-console.log("Any text");
 
+console.log("Any text");
 alert("message") // внутри редактора кода работать не будет
 
 
