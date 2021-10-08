6f8d79b994eeeb1869a8ba279d6b5b5df617b09e
Bisultanova Tanzila <tanzbis81@gmail.com>
2021-10-08 17:19:35 +0300

    Donee

--- script.js
+++ script.js
@@ -1,41 +1,71 @@
-'use strict'
+'use strict';
+//Блок объявления переменных
 let title = prompt("Как называется ваш проект?");
-console.log((title));
-console.log(typeof title);
 let screens = prompt("Какие типы экранов нужно разработать?").toLowerCase();
 const forScreens = screens.split(", ");
-console.log(forScreens);
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
-console.log((screenPrice));
 let adaptive = confirm("Нужен ли адаптив?");
-console.log((adaptive));
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
-console.log(service1);
 let servicePrice1 = +prompt("Cколько это будет стоить?");
-console.log(servicePrice1);
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
-console.log(service2);
 let servicePrice2 = +prompt("Cколько это будет стоить?");
-console.log(servicePrice2);
 let rollback = 20;
 let fullPrice = (screenPrice + servicePrice1 + servicePrice2);
-let servicePercentprice = Math.ceil(fullPrice - (fullPrice * rollback/100));
-console.log((servicePercentprice));
-if (fullPrice > 30000)
-{
-  console.log("Даем скидку в 10%");
-} else if (fullPrice <= 30000 && fullPrice > 15000) {
-    console.log("Даем скидку в 5%");
-} else if (15000 <= fullPrice &&  fullPrice > 0 ) {
-    console.log("Скидка не предусмотрена");
-} else if(fullPrice < 0) {
-    console.log("что-то пошло не так");
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * rollback/100));
+let allServicePrices;
+
+//Блок описания функций
+const showTypeOf = function(variable) {
+    console.log(variable, typeof variable);
+};
+const getRollbackMessage = function(price) {
+
+    if (price > 30000) {
+        return "Даем скидку в 10%";
+    } else if (price <= 30000 && price > 15000) {
+        return "Даем скидку в 5%";
+    } else if (15000 <= price &&  price > 0 ) {
+        return "Скидка не предусмотрена";
+    } else if(price < 0) {
+        return "что-то пошло не так";
+    }
+
+};
+
+const getAllServicePrices = function(service1, service2) {
+    return service1 + service2;
+};
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+
+function getFullPrice(screenPrice, allServicePrices) {
+    return screenPrice + allServicePrices;
 }
+fullPrice = getAllServicePrices(screenPrice, allServicePrices);
 
-console.log("Any text");
-alert("message") // внутри редактора кода работать не будет
+const getTitle = function(title) {
+    if (title[0] === ' ') {
+        return title[1].toUpperCase() + title.slice(2).toLowerCase();
+    } else {
+        return title[0].toUpperCase() + title.slice(1).toLowerCase();
+    }
+
+};
+
+const getAllServicePercentPrice = function(a, b, c) {
+    return  Math.ceil(a - (b * c/100));
+};
+servicePercentPrice = getAllServicePrices(fullPrice, fullPrice, rollback);
 
 
+// Функциональный блок
+console.log(getTitle(title));
+console.log(getRollbackMessage(fullPrice));
+console.log(getAllServicePrices(servicePercentPrice));
+// showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
 
+//Блок мусора
 
+console.log(forScreens);
 
